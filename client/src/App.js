f5291653e42799d96eb75415ce913da0434314e7
Abhishek Chaudhary <abhishekchaudhary1442016@gmail.com>
2022-08-21 19:07:49 +0930

    appjs header css update

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
+import Header from "./components/Header";
 
 function App() {
   return (
-    <div className="container">
+    <>
+      <Header />
+    <div className='container'>
       <h1>Hello world</h1>
-      
+  
     </div>
+    </>
   );
 }
 
