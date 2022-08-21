b16323761b6d780797d87cd002a36d9b6d1dbda1
Abhishek Chaudhary <abhishekchaudhary1442016@gmail.com>
2022-08-21 16:46:11 +0930

    return projects

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -13,9 +13,33 @@ const ClientType = new GraphQLObjectType({
     })
 });
 
+//Project Type
+const ProjectType = new GraphQLObjectType({
+    name: 'Project',
+    fields : () => ({
+        id: { type: GraphQLID },
+        name: { type: GraphQLString },
+        description: { type: GraphQLString },
+        status: { type: GraphQLString },
+    })
+});
+
 const RootQuery = new GraphQLObjectType({
     name: 'RootQueryType',
     fields: {
+        projects: {
+            type: new GraphQLList (ProjectType),
+            resolve(parent, args) {
+                return projects;
+            }
+        },
+        project: {
+            type: ProjectType,
+            args: { id: { type: GraphQLID } },
+            resolve(parent, args) {
+                return projects.find((project) => project.id === args.id);
+            },
+        },
         clients: {
             type: new GraphQLList (ClientType),
             resolve(parent, args) {
@@ -26,10 +50,10 @@ const RootQuery = new GraphQLObjectType({
             type: ClientType,
             args: { id: { type: GraphQLID } },
             resolve(parent, args) {
-                return clients.find(client => client.id === args.id);
-            }
-        }
-    }
+                return clients.find((client) => client.id === args.id);
+            },
+        },
+    },
 });
 
 module.exports = new GraphQLSchema({
